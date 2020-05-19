705d3556313ea3ce21466219536371a1144c160d
unknown <user@452-JY2F-9226.nknush.kh.edu.tw>
2020-05-19 12:02:44 +0800

    0519

--- bigfile.py
+++ bigfile.py
@@ -3,7 +3,19 @@ import pandas as pd
 import os, json
 ## C:\Users\user\Downloads
 
-filename = os.path.join("C:/", "Users", "user", "Downloads", 'C-B0024-002.json')
+#filename = os.path.join("C:/", "Users", "user", "Downloads", 'C-B0024-002.json')
+#filename = os.path.join("D:/", "Download", 'C-B0024-002.json')
+
+filename = os.path.join(os.sep, "mnt","d", "Download", 'C-B0025-002', 'dy_Report_2019.json')
+
 with open(filename, 'r', encoding='UTF-8') as f:
     datas = json.loads(f.read())
-print(len(datas))
+
+locations = datas['cwbdata']['resources'][0]['resource'][0]['data'][0]['surfaceObs'][0]['location']
+
+#for location in locations:
+location = locations[0]
+stationObsTime = location['stationObsTimes'][0]['stationObsTime']
+print(location['station'][0]['stationName'], len(stationObsTime))
+for day in stationObsTime:
+    print(day['dataDate'][0], day['weatherElements'][0]['precipitation'][0])
