0684dcda23f46dcb19a0239e832935bf9d6e01f3
Your Name <you@example.com>
2020-05-22 16:22:15 +0800

    0522

--- d086.py
+++ d086.py
@@ -1,11 +1,17 @@
-s = input().strip()
-s = s.upper()
+ta = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-table = 'ABCDEFG'
-ans = 0
-for c in s:
-    if c not in table:
-        print('Fail')
-        break
-    ans = ans + table.find(c)+1
+while True:
+	s = input().strip()
+	if s == '0':
+		break
     
+	s = s.upper()
+	ans = 0
+	for c in s:
+		if c not in ta:
+			print('Fail')
+			break
+
+		ans += ta.find(c)+1
+	else:
+		print(ans)
