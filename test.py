3493111251df14fbb668fec863945e629c2b1632
jiangsir <jiangzero@gmail.com>
2020-12-31 11:30:39 +0800

    bebras 群發

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-import os, sys, smtplib
+import os
+import sys
+import smtplib
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
@@ -10,15 +12,16 @@ subject = '高師大附中 109學年度國際運算思維挑戰賽 挑戰證明'
 from_addr = "mailer@zerojudge.tw"
 app_passwd = "zridfkvaolgtoekp"
 to_addr = [""]
-# 檔案位置 在windows底下記得要加上r 如下 要完整的路徑
-attachePath = 'C:\\Users\\user\\Downloads\\grades-高一忠\\'
-attachments = os.listdir(attachePath)
+to_domain = 'stu.nknush.kh.edu.tw'
+
+# attachePath = 'C:\\Users\\user\\Downloads\\109Bebras成績證明\\'
+attachePath = '/mnt/c/Users/user/Downloads/109Bebras成績證明/'
+attachments = sorted(os.listdir(attachePath))  # 取得成績證明資料夾的所有檔案
 #attachments = [attachePath+name for name in os.listdir(attachePath)]
 
+
 def main():
-    # sender = '你的EMAIL'
-    # gmail_password = '你的EMAIL密碼'
-    # recipients = ['收件人01的EMAIL','收件人02的EMAIL']
+    # html 格式的信件內容
     htmlcontent = """
     <!doctype html>
     <html>
@@ -32,20 +35,20 @@ def main():
     </html>
     """
 
-    # 加入檔案到MAIL底下
+    # 讀取所有的挑戰證明檔案
     for filename in attachments:
         # 建立郵件主題
-        content = MIMEMultipart()  #建立MIMEMultipart物件
-        content["subject"] = subject  #郵件標題
-        content["from"] = from_addr  #寄件者
+        content = MIMEMultipart()  # 建立MIMEMultipart物件
+        content["subject"] = subject  # 郵件標題
+        content["from"] = from_addr  # 寄件者
+        to_addr[0] = filename.split('.')[0] + to_domain  # 收件者 email
+        content["to"] = to_addr[0]  # 收件者
 
         content.attach(MIMEText(htmlcontent, 'html'))
-        to_addr[0] = filename.split('.')[0]+'@gmail.com'
-        content["to"] = to_addr[0] #收件者
 
         try:
             with open(attachePath+filename, 'rb') as fp:
-                print('can read faile filename=',filename, 'fp=', fp)
+                print('讀取 filename=', fp.name)
                 msg = MIMEBase('application', "octet-stream")
                 msg.set_payload(fp.read())
 
@@ -68,7 +71,7 @@ def main():
         if status == {}:
             print(' --- 郵件傳送成功!')
         else:
-            print(' ---', status)
+            print(' --- 失敗', status)
 
 
 if __name__ == '__main__':
